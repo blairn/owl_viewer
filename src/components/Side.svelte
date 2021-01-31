f8c379be808c2f6298ba853b0ae2197a37063f4c
Blair Nilsson <blair.nilsson@gmail.com>
2021-01-31 13:55:40 +1300

    init

---
+++ src/components/Side.svelte
@@ -0,0 +1,29 @@
+<script>
+</script>
+
+<style lang="scss">
+
+.sidebar {
+  height: 100%;
+  width: 40px;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #08080F;
+  overflow-x: hidden;
+  padding-top: 20px;
+}
+
+</style>
+
+<div class='sidebar'>
+  <svg width=32 height=32 viewbox="-1 -1 2 2">
+    <circle cx="0" cy="0" r="0.9" stroke="white" stroke-width="0.1"></circle>
+    <circle cx="0" cy="0" r="0.7" fill="white"></circle>
+  </svg><br>
+  <svg width=32 height=32 viewbox="-1 -1 2 2">
+    <circle cx="0" cy="0" r="0.9" stroke="white" stroke-width="0.1"></circle>
+    <circle cx="0" cy="0" r="0.7" fill="white"></circle>
+  </svg><br>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,36 @@
+<script>
+	import Banner from '$components/Banner.svelte'
+	import Side from '$components/Side.svelte'
+</script>
+
+<style lang="scss">
+
+	$eigenlicht-color: #16161d;
+	:root {
+		font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+		background-color: $eigenlicht-color;
+	}
+	
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+	}
+	@media (min-width: 480px) {
+		h1 {
+			max-width: none;
+		}
+
+		p {
+			max-width: none;
+		}
+	}
+</style>
+
+<Banner></Banner>
+
+<Side></Side>
+
+<main>
+</main>
+
